552a3e2b703b596bd46078cde87573751ffac72f
jrector <jrector@mindstreaminteractive.com>
2015-11-05 17:14:28 -0500

    Working on sass file structure

--- app/index.js
+++ app/index.js
@@ -50,6 +50,13 @@ var MSGenerator = yeoman.generators.Base.extend({
         this.mkdir('source/styles/utils');
         this.mkdir('source/styles/utils/functions');
         this.mkdir('source/styles/utils/mixins');
+        this.mkdir('source/styles/components');
+        this.mkdir('source/styles/components/atoms');
+        this.mkdir('source/styles/components/molecules');
+        this.mkdir('source/styles/components/organisms');
+        this.mkdir('source/styles/components/templates');
+        this.mkdir('source/styles/components/pages');
+        this.mkdir('source/styles/vendor');
         this.mkdir('source/views');
         this.mkdir('gulp');
 
@@ -84,18 +91,34 @@ var MSGenerator = yeoman.generators.Base.extend({
 
         // Copy script and sass files
         this.copy('source/scripts/_main.js', 'source/scripts/main.js');
-        this.copy('source/styles/_vars.scss', 'source/styles/_vars.scss');
         this.copy('source/styles/_main.scss', 'source/styles/main.scss');
         this.copy('source/styles/global/_base.scss', 'source/styles/global/_base.scss');
         this.copy('source/styles/global/_normalize.scss', 'source/styles/global/_normalize.scss');
         this.copy('source/styles/global/_shame.scss', 'source/styles/global/_shame.scss');
         this.copy('source/styles/global/_trumps.scss', 'source/styles/global/_trumps.scss');
+        this.copy('source/styles/global/_vars.scss', 'source/styles/global/_vars.scss');
         this.copy('source/styles/utils/functions/_map-getters.scss', 'source/styles/utils/functions/_map-getters.scss');
         this.copy('source/styles/utils/functions/_valid-value.scss', 'source/styles/utils/functions/_valid-value.scss');
         this.copy('source/styles/utils/mixins/_attention.scss', 'source/styles/utils/mixins/_attention.scss');
         this.copy('source/styles/utils/mixins/_clearfix.scss', 'source/styles/utils/mixins/_clearfix.scss');
         this.copy('source/styles/utils/mixins/_hide-text.scss', 'source/styles/utils/mixins/_hide-text.scss');
         this.copy('source/styles/utils/mixins/_reset.scss', 'source/styles/utils/mixins/_reset.scss');
+        this.copy('source/styles/components/atoms/_buttons.scss', 'source/styles/components/atoms/_buttons.scss');
+        this.copy('source/styles/components/atoms/_headings.scss', 'source/styles/components/atoms/_headings.scss');
+        this.copy('source/styles/components/atoms/_inputs.scss', 'source/styles/components/atoms/_inputs.scss');
+        this.copy('source/styles/components/atoms/_labels.scss', 'source/styles/components/atoms/_labels.scss');
+        this.copy('source/styles/components/atoms/_links.scss', 'source/styles/components/atoms/_links.scss');
+        this.copy('source/styles/components/atoms/_lists.scss', 'source/styles/components/atoms/_lists.scss');
+        this.copy('source/styles/components/atoms/_logo.scss', 'source/styles/components/atoms/_logo.scss');
+        this.copy('source/styles/components/atoms/_paragraph.scss', 'source/styles/components/atoms/_paragraph.scss');
+        this.copy('source/styles/components/atoms/_radio.scss', 'source/styles/components/atoms/_radio.scss');
+        this.copy('source/styles/components/atoms/_select.scss', 'source/styles/components/atoms/_select.scss');
+        this.copy('source/styles/components/atoms/_table.scss', 'source/styles/components/atoms/_table.scss');
+        this.copy('source/styles/components/atoms/_textarea.scss', 'source/styles/components/atoms/_textarea.scss');
+        this.copy('source/styles/components/molecules/_form.scss', 'source/styles/components/molecules/_form.scss');
+        this.copy('source/styles/components/molecules/_navigation.scss', 'source/styles/components/molecules/_navigation.scss');
+        this.copy('source/styles/components/organisms/_header.scss', 'source/styles/components/organisms/_header.scss');
+        this.copy('source/styles/components/organisms/_footer.scss', 'source/styles/components/organisms/_footer.scss');
 
         // For files that need info from yeoman, use .template instead of .copy.
         var context = {
