6d29afd427399c247e6b1fdb7825ec401bbb209d
Sean Sefcik <ssefcik@gmail.com>
2016-04-12 17:00:53 -0400

    Updates Browserify/Babel Gulp tasks.

--- app/index.js
+++ app/index.js
@@ -51,12 +51,13 @@ var MSGenerator = yeoman.generators.Base.extend({
         this.mkdir('source/styles/utils/functions');
         this.mkdir('source/styles/utils/mixins');
         this.mkdir('source/styles/components');
-        this.mkdir('source/styles/components/atoms');
-        this.mkdir('source/styles/components/molecules');
-        this.mkdir('source/styles/components/organisms');
+        this.mkdir('source/styles/components/basics');
+        this.mkdir('source/styles/components/fragments');
+        this.mkdir('source/styles/components/modules');
         this.mkdir('source/styles/components/templates');
         this.mkdir('source/styles/components/pages');
         this.mkdir('source/styles/vendor');
+        this.mkdir('source/images');
         this.mkdir('source/views');
         this.mkdir('gulp');
 
@@ -87,7 +88,6 @@ var MSGenerator = yeoman.generators.Base.extend({
         this.copy('gulp/_images.js', 'gulp/images.js');
         this.copy('gulp/_scripts.js', 'gulp/scripts.js');
         this.copy('gulp/_styles.js', 'gulp/styles.js');
-        this.copy('gulp/_watching.js', 'gulp/watching.js');
 
         // Copy script and sass files
         this.copy('source/scripts/_main.js', 'source/scripts/main.js');
@@ -105,22 +105,23 @@ var MSGenerator = yeoman.generators.Base.extend({
         this.copy('source/styles/utils/mixins/_clearfix.scss', 'source/styles/utils/mixins/_clearfix.scss');
         this.copy('source/styles/utils/mixins/_hide-text.scss', 'source/styles/utils/mixins/_hide-text.scss');
         this.copy('source/styles/utils/mixins/_reset.scss', 'source/styles/utils/mixins/_reset.scss');
-        this.copy('source/styles/components/atoms/_buttons.scss', 'source/styles/components/atoms/_buttons.scss');
-        this.copy('source/styles/components/atoms/_headings.scss', 'source/styles/components/atoms/_headings.scss');
-        this.copy('source/styles/components/atoms/_inputs.scss', 'source/styles/components/atoms/_inputs.scss');
-        this.copy('source/styles/components/atoms/_labels.scss', 'source/styles/components/atoms/_labels.scss');
-        this.copy('source/styles/components/atoms/_links.scss', 'source/styles/components/atoms/_links.scss');
-        this.copy('source/styles/components/atoms/_lists.scss', 'source/styles/components/atoms/_lists.scss');
-        this.copy('source/styles/components/atoms/_logo.scss', 'source/styles/components/atoms/_logo.scss');
-        this.copy('source/styles/components/atoms/_paragraph.scss', 'source/styles/components/atoms/_paragraph.scss');
-        this.copy('source/styles/components/atoms/_radio.scss', 'source/styles/components/atoms/_radio.scss');
-        this.copy('source/styles/components/atoms/_select.scss', 'source/styles/components/atoms/_select.scss');
-        this.copy('source/styles/components/atoms/_table.scss', 'source/styles/components/atoms/_table.scss');
-        this.copy('source/styles/components/atoms/_textarea.scss', 'source/styles/components/atoms/_textarea.scss');
-        this.copy('source/styles/components/molecules/_form.scss', 'source/styles/components/molecules/_form.scss');
-        this.copy('source/styles/components/molecules/_navigation.scss', 'source/styles/components/molecules/_navigation.scss');
-        this.copy('source/styles/components/organisms/_header.scss', 'source/styles/components/organisms/_header.scss');
-        this.copy('source/styles/components/organisms/_footer.scss', 'source/styles/components/organisms/_footer.scss');
+        this.copy('source/styles/utils/mixins/_spacing.scss', 'source/styles/utils/mixins/_spacing.scss');
+        this.copy('source/styles/components/basics/_buttons.scss', 'source/styles/components/basics/_buttons.scss');
+        this.copy('source/styles/components/basics/_headings.scss', 'source/styles/components/basics/_headings.scss');
+        this.copy('source/styles/components/basics/_inputs.scss', 'source/styles/components/basics/_inputs.scss');
+        this.copy('source/styles/components/basics/_labels.scss', 'source/styles/components/basics/_labels.scss');
+        this.copy('source/styles/components/basics/_links.scss', 'source/styles/components/basics/_links.scss');
+        this.copy('source/styles/components/basics/_lists.scss', 'source/styles/components/basics/_lists.scss');
+        this.copy('source/styles/components/basics/_logo.scss', 'source/styles/components/basics/_logo.scss');
+        this.copy('source/styles/components/basics/_paragraph.scss', 'source/styles/components/basics/_paragraph.scss');
+        this.copy('source/styles/components/basics/_radio.scss', 'source/styles/components/basics/_radio.scss');
+        this.copy('source/styles/components/basics/_select.scss', 'source/styles/components/basics/_select.scss');
+        this.copy('source/styles/components/basics/_table.scss', 'source/styles/components/basics/_table.scss');
+        this.copy('source/styles/components/basics/_textarea.scss', 'source/styles/components/basics/_textarea.scss');
+        this.copy('source/styles/components/fragments/_form.scss', 'source/styles/components/fragments/_form.scss');
+        this.copy('source/styles/components/fragments/_navigation.scss', 'source/styles/components/fragments/_navigation.scss');
+        this.copy('source/styles/components/modules/_header.scss', 'source/styles/components/modules/_header.scss');
+        this.copy('source/styles/components/modules/_footer.scss', 'source/styles/components/modules/_footer.scss');
 
         // For files that need info from yeoman, use .template instead of .copy.
         var context = {
@@ -132,12 +133,14 @@ var MSGenerator = yeoman.generators.Base.extend({
             this.template('source/views/templates/_master.jade', 'source/views/templates/master.jade', context);
         } else {
             this.template('source/views/partials/_head.html', 'source/views/partials/_head.html', context);
+            this.template('gulp/_watching.js', 'gulp/watching.js', context);
         }
 
         this.template('gulp/_templates.js', 'gulp/templates.js', context);
 
         // Copy npm and git files
         this.copy('_.gitignore', '.gitignore');
+        this.copy('_.eslintrc.js', '.eslintrc.js');
         this.copy('_README.md', 'README.md');
 
         if (this.use_Jade) {
